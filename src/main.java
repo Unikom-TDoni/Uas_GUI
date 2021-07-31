6532a2d1772c443b52fbaacd1c1a8d8e967b0b4b
Aris Prabowo <aris@hijrstudio.com>
2021-07-31 15:13:47 +0700

    initial push

---
+++ src/main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Aris Prabowo
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        FormUtama FormUtama = new FormUtama(); 
+        FormUtama.setVisible(true);
+    }
+    
+}
